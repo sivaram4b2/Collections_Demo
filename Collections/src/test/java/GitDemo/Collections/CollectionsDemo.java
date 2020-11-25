5cca5edd6f02dacea40f4d0c2a1ff943d38c2a8c
sivaram4b2 <prathisrk@gmail.com>
2020-11-25 11:55:25 +0530

    added string

--- Collections/src/test/java/GitDemo/Collections/CollectionsDemo.java
+++ Collections/src/test/java/GitDemo/Collections/CollectionsDemo.java
@@ -5,7 +5,7 @@ public class CollectionsDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String name;
+		String siva;
 		
 		System.out.println("Git Demo");
 		
