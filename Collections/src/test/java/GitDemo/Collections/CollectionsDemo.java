14e9236dc8820843715ce9a5251a46290576e698
sivaram4b2 <prathisrk@gmail.com>
2020-11-25 10:42:54 +0530

    First commit

---
+++ Collections/src/test/java/GitDemo/Collections/CollectionsDemo.java
@@ -0,0 +1,12 @@
+package GitDemo.Collections;
+
+public class CollectionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git Demo");
+
+	}
+
+}
